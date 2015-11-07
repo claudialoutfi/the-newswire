3a05137d59424800c79a2b2360317e650915aded
claudialoutfi <loutficlaudia@gmail.com>
2015-11-07 18:42:00 -0500

    Fixed Latest News bug

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
     <div id="content" class="clearfix">
     	<div id="slide-wrap">
-        	<?php 
+        	<?php
                 $args = array(
                     'posts_per_page' => 10,
 					'post_status' => 'publish',
@@ -11,11 +11,11 @@
                 $fPosts = new WP_Query( $args );
 				$countPosts = $fPosts->found_posts;
               ?>
-			  
+
 			<?php if ( $fPosts->have_posts() ) : ?>
-            
+
             <div id="load-cycle"></div>
-              <div class="cycle-slideshow" <?php 
+              <div class="cycle-slideshow" <?php
 				  	if ( get_theme_mod('newswire_slider_effect') ) {
 						echo 'data-cycle-fx="' . wp_kses_post( get_theme_mod('newswire_slider_effect') ) . '" data-cycle-tile-count="10"';
 					} else {
@@ -29,52 +29,52 @@
 						echo 'data-cycle-timeout="5000"';
 					}
 				  ?>>
-            
+
             <div class="cycle-pager"></div>
             <?php while ( $fPosts->have_posts() ) : $fPosts->the_post();  ?>
-			 
+
 			<div class="slides">
             <h2 class="slide-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'newswire' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
               <div id="post-<?php the_ID(); ?>" <?php post_class('post-theme'); ?>>
                  <?php if ( has_post_thumbnail()) : ?>
                     <div class="slide-thumb"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( array(1000, 640) ); ?></a></div>
                  <?php else : ?>
-                 
+
                     <?php $postimgs =& get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
                     if ( !empty($postimgs) ) :
                         $firstimg = array_shift( $postimgs );
                         $my_image = wp_get_attachment_image( $firstimg->ID, array( 1000, 640 ), false );
                     ?>
                     <div class="slide-thumb"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php echo $my_image; ?></a></div>
-                    
+
                     <?php else : ?>
-                    
+
                     <div class="slide-noimg">
                     	<div class="slide-noimg-copy">
 							<?php echo newswire_excerpt(30); ?><br /><br />
                             <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php _e('Continue Reading &rarr;', 'newswire'); ?></a>
                         </div>
                     </div>
-                    
+
                    <?php endif; ?>
-                   
-                 <?php endif; ?>					
+
+                 <?php endif; ?>
               </div>
             </div>
-            
+
  			<?php endwhile; ?>
 
             </div>
-            <?php endif; ?> 
+            <?php endif; ?>
 
             </div>
-        
+
         <div id="main" class="col620 clearfix" role="main">
-        
+
 			<?php if ( have_posts() ) : ?>
-            
-            <h2 class="heading-latest"><?php _e('Latest News', 'newswire'); ?></h2>
-            	
+
+            <h2 class="heading-latest"><?php _e('Articles récents', 'newswire'); ?></h2>
+
                 <div id="grid-wrap" class="clearfix">
 
 				<?php /* Start the Loop */ ?>
@@ -91,12 +91,12 @@
                     </div>
 
 				<?php endwhile; ?>
-                
+
                 </div>
 
 				<?php if (function_exists("newswire_pagination")) {
-							newswire_pagination(); 
-				} elseif (function_exists("newswire_content_nav")) { 
+							newswire_pagination();
+				} elseif (function_exists("newswire_content_nav")) {
 							newswire_content_nav( 'nav-below' );
 				}?>
 
@@ -120,5 +120,5 @@
         <?php get_sidebar('home'); ?>
 
     </div> <!-- end #content -->
-        
+
 <?php get_footer(); ?>
